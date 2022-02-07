f63341574a8b813b3c8febc0ca5d576990b74220
Jhonny Zamora <jhonnyzamora@outlook.com>
2022-02-07 16:00:26 -0500

    Update PostRequest.ino

--- PostRequest/PostRequest.ino
+++ PostRequest/PostRequest.ino
@@ -1,22 +1,23 @@
 /*****************************************************************************
-  
-  This is an example for doing a simple Post Request in JSON format with
-  a microcontroller [WiFi] ESP32 of Espressif Systems.
 
-  In this example we send a region or timezone similar to 'America/Guayaquil' 
+  This is an example for doing a simple Post Request in JSON format with
+  a microcontroller [WiFi] ESP8266 of Espressif Systems.
+
+  In this example we send a region or timezone similar to 'America/Guayaquil'
   for received a string on JSON format with the local time and other dates
-  provided for a micro service of https://binaryec.com  
-  
+  provided for a micro service of https://binaryec.com
+
   Developer: Jhonny Zamora A.
   https://github.com/jbzamora
   Email: jzamora@binaryec.com
-  
-*****************************************************************************/
-#include <WiFi.h>
-#include <HTTPClient.h>
 
-const char* ssid = "YOUR_SSID";
-const char* password = "YOUR_PASS";
+*****************************************************************************/
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "SPD";
+const char* password = "SOLOPARADIOSES";
 
 String timezone = "America/Guayaquil";
 
@@ -44,26 +45,27 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {    
-    String json = "{\"timezone\":\"" + timezone + "\"}";
+void loop() {
+  String json = "{\"timezone\":\"" + timezone + "\"}";
 
-    HTTPClient http;
+  WiFiClient client;  
+  HTTPClient http;
 
-    http.begin("https://binaryec.com/timezone/index.php");      //Specify destination for HTTP request
-    http.addHeader("Content-Type", "application/json");         //Specify content-type header
+  http.begin(client, "http://binaryec.com/timezone/index.php");      //Specify destination for HTTP request
+  http.addHeader("Content-Type", "application/json");         //Specify content-type header
 
-    int httpResponseCode = http.POST(json);   //Send actual POST request
+  int httpResponseCode = http.POST(json);   //Send actual POST request
 
-    if (httpResponseCode > 0) {
-      String response = http.getString();   //Get the response to the request
-      Serial.print("Code: "); Serial.println(httpResponseCode);     //Print return code
-      Serial.print("Server Response: "); Serial.println(response);  //Print request answer
-    }
-    else {
-      Serial.print("Error on sending POST: ");
-      Serial.println(httpResponseCode);
-    }
-    http.end();  //Free resources
+  if (httpResponseCode > 0) {
+    String response = http.getString();   //Get the response to the request
+    Serial.print("Code: "); Serial.println(httpResponseCode);     //Print return code
+    Serial.print("Server Response: "); Serial.println(response);  //Print request answer
+  }
+  else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();  //Free resources
 
-    delay(5000);
+  delay(5000);
 }
