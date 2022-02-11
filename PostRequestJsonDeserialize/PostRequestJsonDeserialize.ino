26842a1b8dfb1841cd05a0cd937b69988b6b4e9b
Jhonny Zamora <jhonnyzamora@outlook.com>
2022-02-10 23:26:18 -0500

    Update PostRequestJsonDeserialize.ino

--- PostRequestJsonDeserialize/PostRequestJsonDeserialize.ino
+++ PostRequestJsonDeserialize/PostRequestJsonDeserialize.ino
@@ -17,7 +17,10 @@
 #include <WiFiClientSecureBearSSL.h>
 #include <ArduinoJson.h>
 
-const uint8_t fingerprint[20] = {0x9F, 0xEE, 0x15, 0xFD, 0x50, 0x46, 0xF0, 0x45, 0xBD, 0xCB, 0x5C, 0x60, 0xDE, 0x0F, 0xE3, 0x21, 0x0B, 0x8C, 0x8A, 0x76};
+const uint8_t fingerprint[20] =  {0x9F, 0xEE, 0x15, 0xFD, 0x50, 
+                                  0x46, 0xF0, 0x45, 0xBD, 0xCB, 
+                                  0x5C, 0x60, 0xDE, 0x0F, 0xE3, 
+                                  0x21, 0x0B, 0x8C, 0x8A, 0x76};
 
 const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_PASS";
@@ -55,12 +58,12 @@ void loop() {
   client->setFingerprint(fingerprint);
   HTTPClient http;
   http.begin(*client, "https://binaryec.com/timezone/index.php");
-  http.addHeader("Content-Type", "application/json");         //Specify content-type header
+  http.addHeader("Content-Type", "application/json");   //Specify content-type header
 
-  int httpResponseCode = http.POST(json);   //Send actual POST request
+  int httpResponseCode = http.POST(json);               //Send actual POST request
 
   if (httpResponseCode > 0) {
-    String response = http.getString();   //Get the response to the request
+    String response = http.getString();                 //Get response to request
     Serial.print("Code: "); Serial.println(httpResponseCode);     //Print return code
     Serial.print("Server Response: "); Serial.println(response);  //Print request answer
 
